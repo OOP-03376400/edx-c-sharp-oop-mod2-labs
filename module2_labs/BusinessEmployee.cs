9c399278efe706d738e9e2583cc510a57a7336ee
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2018-04-27 22:47:47 -0500

    lab 1 complete

---
+++ module2_labs/BusinessEmployee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace module2_labs
+{
+    public class BusinessEmployee : Employee
+    {
+        public BusinessEmployee(string name) : base(name, 50000)
+        {
+        }
+
+        //creates double var called bonusBudget and assigns value to 1000
+        public double bonusBudget = 1000;
+
+        //this method returns the toString() method - which is the employee's ID number and name - and prints the bonus budget
+        public override string employeeStatus()
+        {
+            return toString() + " with a budget of " + this.bonusBudget;
+        }
+
+
+    }
+}
--- module2_labs/Employee.cs
+++ module2_labs/Employee.cs
@@ -3,8 +3,90 @@ namespace module2_labs
 {
     public class Employee
     {
-        public Employee()
+        //private member variables
+        private string employeeName;
+        private double employeeBaseSalary;
+        private int employeeId;
+
+        //Public properties
+        public string Name
         {
+            get
+            {
+                return employeeName;
+            }
+
+            set
+            {
+                employeeName = value;
+            }
         }
+
+        public double BaseSalary
+        {
+            get
+            {
+                return employeeBaseSalary;
+            }
+
+            set
+            {
+                employeeBaseSalary = value;
+            }
+        }
+
+        public int ID
+        {
+            get
+            {
+                return employeeId;
+            }
+
+            set
+            {
+                employeeId = value;
+            }
+        }
+
+        //creates integer variable called employeeCount and assigns value to 1
+        private static int employeeCount = 1;
+
+        //constructor
+        public Employee(string name, double baseSalary)
+        {
+            this.Name = name;
+            this.BaseSalary = baseSalary;
+            this.ID = employeeCount++;
+        }
+
+        //method to return employee base salary
+        public double getBaseSalary()
+        {
+            return this.BaseSalary;
+        }
+
+        // This method returns the employee name
+        public string getName()
+        {
+            return this.Name;
+        }
+        //this method returns the employee's ID
+        public int getEmployeeID()
+        {
+            return this.ID;
+        }
+
+        //this method returns the employee's ID and Name
+        public string toString()
+        {
+            return this.ID + " " + this.Name;
+        }
+
+        //this method returns the employee's ID and confirms that the employee is in the system
+        public virtual string employeeStatus()
+        {
+            return toString() + " is in the company's system";
+        }
+
     }
 }
